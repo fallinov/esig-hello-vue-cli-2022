12aca3f7af3778cfd26dd26f81c60292ef3a8b36
SFA <steve.fallet@divtec.ch>
2022-03-23 10:53:10 +0100

    création composant ProduitsView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+    <strong>toto</strong>
     <nav>
       <router-link to="/">Home</router-link> |
+      <router-link to="/produits">Produits</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
---
+++ src/views/ProduitsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Produits</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProduitsView",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
